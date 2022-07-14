cf7dee42e25d6e332052babe424a6892a2cfdc92
Oleksandra Boiko <oboiko@atlassian.com>
2022-07-14 12:29:12 -0400

    readme changed, unnessessary data deleted

--- src/User.tsx
+++ src/User.tsx
@@ -7,10 +7,5 @@ export const User = ({ user }: any) => (
     <td>{user.phone}</td>
     <td>{`${user.address.street}   ${user.address.suite} ${user.address.city}`}</td>
     <td>{user.website}</td>
-
-    {/* <td>
-      <User user={todo.user} />
-    </td>
-    <td>{todo.completed ? "completed" : "not completed"}</td> */}
   </tr>
 );
